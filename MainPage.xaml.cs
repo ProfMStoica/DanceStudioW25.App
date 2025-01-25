3b755fade141feda527af7d8a04e15c7ad148cec
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-01-24 19:47:01 -0500

    Re-implement the single-button animation for comparison.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,34 +11,27 @@ public partial class MainPage : ContentPage
     {
         //Display the image and the action buttons
         _imgAvatar.IsVisible = true;
-        _btnStart.IsVisible = true;
-        _btnStart.IsEnabled = true;
-        _btnStop.IsVisible = true;
-        _btnStop.IsEnabled = false;
+        _btnAction.IsVisible = true;
     }
 
     private void OnToggleDancing(object sender, EventArgs e)
     {
         //Toggle the animation of the avatar image based on its current stage and update the action button
-        //Determine  which button has triggered the event handler
-        Button btnAction = (Button)sender;
-        if (btnAction == _btnStop)
+        if (_imgAvatar.IsAnimationPlaying)
         {
             //Stop the animation
             _imgAvatar.IsAnimationPlaying = false;
             
             //Allow the user to start the animation again
-            _btnStop.IsEnabled = false;
-            _btnStart.IsEnabled = true;
+            _btnAction.Text = "Start Dancing";
         }
-        else if (btnAction == _btnStart)
+        else
         {
             //The animation is not playing. Start the animation
             _imgAvatar.IsAnimationPlaying = true;
             
             //Allow the user to stop the animation
-            _btnStop.IsEnabled = true;
-            _btnStart.IsEnabled = false;
+            _btnAction.Text = "Stop Dancing";
         }
     }
 }
