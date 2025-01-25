28d3831e5fa4faebba42a46e6091bc6fa26aa673
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-01-24 19:40:34 -0500

    Implement the Dance Studio using two buttons.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,43 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    private void OnWelcomeToDanceStudio(object sender, EventArgs e)
+    {
+        //Display the image and the action buttons
+        _imgAvatar.IsVisible = true;
+        _btnStart.IsVisible = true;
+        _btnStart.IsEnabled = true;
+        _btnStop.IsVisible = true;
+        _btnStop.IsEnabled = false;
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnToggleDancing(object sender, EventArgs e)
+    {
+        //Toggle the animation of the avatar image based on its current stage and update the action button
+        //Determine  which button has triggered the event handler
+        Button btnAction = (Button)sender;
+        if (btnAction == _btnStop)
+        {
+            //Stop the animation
+            _imgAvatar.IsAnimationPlaying = false;
+            
+            //Allow the user to start the animation again
+            _btnStop.IsEnabled = false;
+            _btnStart.IsEnabled = true;
+        }
+        else if (btnAction == _btnStart)
+        {
+            //The animation is not playing. Start the animation
+            _imgAvatar.IsAnimationPlaying = true;
+            
+            //Allow the user to stop the animation
+            _btnStop.IsEnabled = true;
+            _btnStart.IsEnabled = false;
+        }
+    }
 }
-
